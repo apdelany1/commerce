e5ed5213f6a41b5df6720eff94ed96c0836f30b5
Aidan Delany <apdelany1@gmail.com>
2025-05-02 22:26:48 -0400

    specification #1 started, placeholders and starter/dummy code added that will be altered later when testing other functions. listings, bids, comments started. admin also started with some placeholder text in order to test the admin access. super user created and admin shows correct information from models.py. AuctionListing will be tested next in the create listing function

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import AuctionListing, Bid, Comment, User
 
 # Register your models here.
+
+class AuctionListingAdmin(admin.ModelAdmin):
+    list_display = ("__str__",)
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("__str__",)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("__str__",)
+
+admin.site.register(AuctionListing, AuctionListingAdmin)
+admin.site.register(Bid, BidAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(User)
--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    photo = models.URLField()
+    category = models.CharField(max_length=64)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctionListing", null=True)
+    def __str__(self):
+        return f"{self.title} with the description of {self.description} starting at {self.price} IMG: {self.photo} in category {self.category} created by {self.owner}"
+
+class Bid(models.Model):
+    startingBid = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bid", null=True)
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments", null=True)
+    auctionListing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="comments", null=True)
+    content = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return f"{self.content} said by {self.owner} on {self.auctionListing}"
